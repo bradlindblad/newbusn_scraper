07832c36624228a35e878c3ef73d2fed1297aa5f
BradL <34219856+bradlindblad@users.noreply.github.com>
2018-10-08 07:26:20 -0500

    added Corporate search
    essentially duplicated the loop to run and look for corporate businesses after the partnerships were done running

--- scraper.py
+++ scraper.py
@@ -1,5 +1,4 @@
 
-
 ##### MODULES
 import time
 from datetime import datetime
@@ -31,6 +30,7 @@ def hasXpath(text):
     except:
         return False
 
+# ######### LLP
 
 ##### START LETTER LOOP
 for letter in alphabet:
@@ -64,14 +64,80 @@ for letter in alphabet:
             founded = founded.replace('Original File Date:', '')
             founded = founded.replace(' ', '')
             founded = datetime.strptime(founded, '%m/%d/%Y')
-
-            town = driver.find_element_by_xpath("//*[@id='BusnSrchFM']/div[1]/div[1]/span[2]").text
-            town = town.split(',')
-            town = town[0]
-            if 'PO' in town:  # sometimes in the principal office section there's an extra row (span) for PO box, this checks that
-                town = driver.find_element_by_xpath("//*[@id='BusnSrchFM']/div[1]/div[1]/span[3]").text
+            try:
+                town = driver.find_element_by_xpath("//*[@id='BusnSrchFM']/div[1]/div[1]/span[2]").text
                 town = town.split(',')
                 town = town[0]
+                if 'PO' in town:  # sometimes in the principal office section there's an extra row (span) for PO box, this checks that
+                    town = driver.find_element_by_xpath("//*[@id='BusnSrchFM']/div[1]/div[1]/span[3]").text
+                    town = town.split(',')
+                    town = town[0]
+            except:
+                pass
+
+            print(busn_name)
+            driver.find_element_by_xpath("//*[@id='BusnSrchFM']/div[1]/p/a").click()
+
+            # Add to DataFrame
+            businesses = businesses.append({'Name': busn_name,
+                                            'Founded': founded,
+                                            'Phone': phone,
+                                            'Town': town}, ignore_index=True)
+
+            i = i + 1
+
+        # Click 'Next' button
+        time.sleep(1)
+        driver.find_element_by_link_text("Next").click()
+
+
+
+    driver.find_element_by_xpath("//*[@id='BusnSrchFM']/div[1]/p[2]/a").click()
+    time.sleep(5)
+
+######### CORP
+
+##### START LETTER LOOP
+for letter in alphabet:
+
+    # ITERATION FOR EACH LETTER OF ALPHABET
+
+    search_box = driver.find_element_by_xpath("//*[@id='searchName']")
+    search_box.click()
+    search_box.clear()
+    search_box.send_keys(letter)
+    search_box.send_keys(Keys.RETURN)
+
+    ##### START PAGE LOOP
+    while hasXpath("Next") is True:
+
+        #### P A G E ####
+        # See how many partnerships on page
+        links = driver.find_elements_by_link_text("Corporation")
+        length = len(links)
+
+        # Iterate through the list with indexing
+        i = 0
+        while i < length:
+            time.sleep(1)
+            driver.find_elements_by_link_text("Corporation")[i].click()
+            time.sleep(1)
+            phone = driver.find_element_by_xpath("//*[@id='BusnSrchFM']/div[1]/table/tbody/tr/td[2]/ol/li[1]").text
+            busn_name = driver.find_element_by_xpath("//*[@id='BusnSrchFM']/div[1]/h3").text
+            founded = driver.find_element_by_xpath("//*[@id='BusnSrchFM']/div[1]/table/tbody/tr/td[1]/ol/li[4]").text
+            founded = founded.replace('Original File Date:', '')
+            founded = founded.replace(' ', '')
+            founded = datetime.strptime(founded, '%m/%d/%Y')
+            try:
+                town = driver.find_element_by_xpath("//*[@id='BusnSrchFM']/div[1]/div[1]/span[2]").text
+                town = town.split(',')
+                town = town[0]
+                if 'PO' in town:  # sometimes in the principal office section there's an extra row (span) for PO box, this checks that
+                    town = driver.find_element_by_xpath("//*[@id='BusnSrchFM']/div[1]/div[1]/span[3]").text
+                    town = town.split(',')
+                    town = town[0]
+            except:
+                pass
 
             print(busn_name)
             driver.find_element_by_xpath("//*[@id='BusnSrchFM']/div[1]/p/a").click()
@@ -91,7 +157,8 @@ for letter in alphabet:
     driver.find_element_by_xpath("//*[@id='BusnSrchFM']/div[1]/p[2]/a").click()
     time.sleep(5)
 
-    # Only keep recent filings
-    d = datetime.today() - timedelta(days=60)  # date 60 days ago
-    businesses = businesses[(businesses['Founded'] > d)]
 
+# Only keep recent filings
+d = datetime.today() - timedelta(days=60)  # date 60 days ago
+businesses = businesses[(businesses['Founded'] > d)]
+businesses.to_csv('businessass.csv')
